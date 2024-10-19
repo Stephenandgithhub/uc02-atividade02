0b7c7da06dd10f824affafee1b8981e217012399
Stephen <stephen.3942@aluno.pr.senac.br>
2024-10-18 21:47:22 -0300

    enough

--- script.js
+++ script.js
@@ -2,12 +2,29 @@
 
 const cards = document.querySelectorAll(".card"); //(Using queryselector insetad of elementtid to get all class card)
 const flips = document.getElementById("flips");
+const timerShow = document.getElementById("time");
+const startButton = document.getElementById("start");
 let count = 0;
 let hasFlipped = false;
 let lockBoard = false; //(one card flip at a time - thank you ai)
 let firstCard, secondCard;
 let matchedCards = 0;
 const totalCards = cards.length / 2;
+let timer;
+let seconds = 0;
+let gameStart = false;
+
+//apply timer
+
+function startTimer() {
+  timer = setInterval(statsTimer, 1000);
+}
+function statsTimer() {
+  seconds++;
+  const minutes = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+  timerShow.textContent = `TImer ${minutes}:${secs} secs`;
+}
 
 //apply if statements (equal = stays; else bye)
 
@@ -35,7 +52,9 @@ function checkForMatch() {
     matchedCards++;
 
     if (matchedCards === totalCards) {
+      clearInterval(timer);
       alert("You won! 😊💐");
+      reset();
       //print("You won! 😊💐"); -> do not use print THIS ISNT C LANGUAGE!
     }
     return;
@@ -49,8 +68,38 @@ function disableCards() {
   secondCard.removeEventListener("click", flipCard);
 }
 
+//win; rest game
+function reset() {
+  count = 0;
+  matchedCards = 0;
+  flips.textContent = "Flips " + count;
+  seconds = 0;
+  timerShow.textContent = "Timer 0:00 sec";
+}
+
 //apply flip action to cards -> nightmare (SPENT 30 MINS FIGURING - I FORGOT TO ADD CARDS FOR EACH 🤧)
 
 cards.forEach((card) => {
+  card.classList.remove("flipped");
   card.addEventListener("click", flipCard);
+});
+
+//apply shuffling
+
+shuffleCards();
+gameStart = false;
+
+function shuffleCards() {
+  cards.forEach((card) => {
+    card.style.order = Math.floor(Math.random() * cards.length);
+  });
+}
+
+//starting timer; reset game
+startButton.addEventListener("click", function () {
+  if (timer) return;
+
+  reset();
+  gameStart = true;
+  startTimer();
 });
